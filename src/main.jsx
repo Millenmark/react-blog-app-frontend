5b23b4e6005bfc465becf772d62754efa4d9ce24
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-26 10:00:17 +0800

    update: added query client

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
