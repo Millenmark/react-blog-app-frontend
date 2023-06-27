2312f6b601ce4c64d765a1b9a7cfd02f8328f362
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-27 10:37:35 +0800

    update: profile dropdown for dashboard and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { useSelector } from "react-redux";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/actions/userActions";
 
 const Header = () => {
+  const menu = ["home", "articles", "pages", "pricing", "faq"];
+
   const [navIsVisible, setNavIsVisible] = useState(false);
+  const [profileDropdown, setProfileDropdown] = useState(false);
   const { userInfo } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const handleNavigationVisibility = () => {
     setNavIsVisible((prev) => !prev);
   };
-
-  const menu = ["home", "articles", "pages", "pricing", "faq"];
 
   return (
     <header className="sticky top-0 left-0 right-0 z-50 bg-white">
@@ -47,7 +52,40 @@ const Header = () => {
             ))}
           </ul>
           {userInfo ? (
-            <></>
+            <div className="flex items-center gap-y-5 flex-col text-white lg:text-dark-soft lg:flex-row gap-x-5 font-semibold">
+              <div className="relative group">
+                <div className="flex flex-col items-center">
+                  <button
+                    className=" flex gap-x-1 items-center border-blue-500 border-2 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all mt-5 lg:mt-0 duration-300 "
+                    onClick={() => setProfileDropdown(!profileDropdown)}
+                  >
+                    <span>Profile</span>
+                    <MdKeyboardArrowDown />
+                  </button>
+                  <div
+                    className={`${
+                      profileDropdown ? "block" : "hidden"
+                    } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                  >
+                    <ul className="bg-dark-soft lg:bg-white text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                      <button
+                        type="button"
+                        className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                      >
+                        Dashboard
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => dispatch(logout())}
+                        className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                      >
+                        Logout
+                      </button>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           ) : (
             <Link to="/register">
               <button className="border-blue-500 border-2 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all mt-5 lg:mt-0 duration-300 ">
