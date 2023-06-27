5d964c1a130dbbb4960141a3b462667830e5ffd6
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-27 09:28:51 +0800

    update: added user reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import xxx from "./reducers/countReducers";
+import user from "./reducers/userReducers";
 
 const store = configureStore({
   reducer: {
     xxx,
+    user,
   },
 });
 
