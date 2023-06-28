76246265f3018d294bc89a4a59ce37912616d55f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 13:32:27 +0800

    fixed: commented out the unnecessary element

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -74,7 +74,7 @@ const Profile = () => {
   return (
     <section className="container mx-auto px-5 py-10">
       <div className="w-full max-w-sm mx-auto">
-        <p>{profileData.name}</p>
+        {/* <p>{profileData.name}</p> this works because of the queryClient.invalidateQueries() */}
         <ProfilePicture avatar={profileData?.avatar} />
         <form onSubmit={handleSubmit(submitHandler)}>
           <div className="flex flex-col mb-6 w-full">
