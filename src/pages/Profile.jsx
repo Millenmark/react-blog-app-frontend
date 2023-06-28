53791061f190897908f4f970a4c37deb098087df
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-28 13:30:04 +0800

    update: added some functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,51 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useQuery } from "@tanstack/react-query";
-import { getUserProfile } from "../services";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getUserProfile, updateProfile } from "../services";
+import { toast } from "react-hot-toast";
+import { useMemo } from "react";
 import ProfilePicture from "../components/ProfilePicture";
+import { setUserInfo } from "../redux/reducers/userReducers";
 
 const Profile = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
   const { userInfo } = useSelector((state) => state.user);
 
-  const {
-    data: profileData,
-    isLoading: profileIsLoading,
-    error: profileError,
-  } = useQuery({
+  const { data: profileData, isLoading: profileIsLoading } = useQuery({
     queryFn: () => {
       return getUserProfile({ token: userInfo.token });
     },
     queryKey: ["profile"],
   });
+
+  const { mutate, isLoading: updateProfileIsLoading } = useMutation({
+    mutationFn: ({ name, email, password }) => {
+      return updateProfile({
+        token: userInfo.token,
+        userData: { name, email, password },
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(setUserInfo(data));
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile is updated");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
 
   const {
     register,
@@ -32,129 +57,121 @@ const Profile = () => {
       email: "",
       password: "",
     },
+    values: useMemo(() => {
+      return {
+        name: profileIsLoading ? "" : profileData.name,
+        email: profileIsLoading ? "" : profileData.email,
+      };
+    }, [profileData?.email, profileData?.name, profileIsLoading]),
     mode: "onChange",
-    values: {
-      name: profileIsLoading ? "" : profileData.name,
-      email: profileIsLoading ? "" : profileData.email,
-    },
   });
 
-  const submitHandler = (data) => {};
-
-  useEffect(() => {
-    if (!userInfo) {
-      navigate("/");
-    }
-  }, [navigate, userInfo]);
+  const submitHandler = (data) => {
+    const { name, email, password } = data;
+    mutate({ name, email, password });
+  };
 
   return (
-    <>
-      <section className="container mx-auto px-5 py-10">
-        <div className="w-full max-w-sm mx-auto">
-          <ProfilePicture avatar={profileData?.avatar} />
-          <form onSubmit={handleSubmit(submitHandler)}>
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="name"
-                className="text-[#5a718] font-semibold block"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                {...register("name", {
-                  required: { value: true, message: "Name is required" },
-                  minLength: {
-                    value: 3,
-                    message: "Name must be at least 3 characters",
-                  },
-                })}
-                placeholder="Enter name"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-1 rounded-lg px-2 py-3 font-semibold block outline-none border ${
-                  errors.name ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.name?.message && (
-                <p className=" text-red-500 text-xs mt-1">
-                  {errors.name?.message}
-                </p>
-              )}
-            </div>
-
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="email"
-                className="text-[#5a718] font-semibold block"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                {...register("email", {
-                  required: { value: true, message: "Email is required" },
-                  pattern: {
-                    value:
-                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: "Please enter a valid email.",
-                  },
-                })}
-                placeholder="Enter email"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-1 rounded-lg px-2 py-3 font-semibold block outline-none border ${
-                  errors.email ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.email?.message && (
-                <p className=" text-red-500 text-xs mt-1">
-                  {errors.email?.message}
-                </p>
-              )}
-            </div>
-
-            <div className="flex flex-col mb-6 w-full">
-              <label
-                htmlFor="password"
-                className="text-[#5a718] font-semibold block"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Password is required",
-                  },
-                  minLength: {
-                    value: 6,
-                    message: "Password length must be at least 6 characters",
-                  },
-                })}
-                placeholder="Enter password"
-                className={`placeholder:text-[#959ead] text-dark-hard mt-1 rounded-lg px-2 py-3 font-semibold block outline-none border ${
-                  errors.password ? "border-red-500" : "border-[#c3cad9]"
-                }`}
-              />
-              {errors.password?.message && (
-                <p className=" text-red-500 text-xs mt-1">
-                  {errors.password?.message}
-                </p>
-              )}
-            </div>
-
-            <button
-              type="submit"
-              disabled={!isValid || profileIsLoading}
-              className="bg-primary text-white font-bold text-lg py-3 px-8 w-full rounded-lg my-6 disabled:opacity-70 disabled:cursor-not-allowed"
+    <section className="container mx-auto px-5 py-10">
+      <div className="w-full max-w-sm mx-auto">
+        <p>{profileData.name}</p>
+        <ProfilePicture avatar={profileData?.avatar} />
+        <form onSubmit={handleSubmit(submitHandler)}>
+          <div className="flex flex-col mb-6 w-full">
+            <label
+              htmlFor="name"
+              className="text-[#5a7184] font-semibold block"
             >
-              Register
-            </button>
-          </form>
-        </div>
-      </section>
-    </>
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              {...register("name", {
+                minLength: {
+                  value: 1,
+                  message: "Name length must be at least 1 character",
+                },
+                required: {
+                  value: true,
+                  message: "Name is required",
+                },
+              })}
+              placeholder="Enter name"
+              className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+                errors.name ? "border-red-500" : "border-[#c3cad9]"
+              }`}
+            />
+            {errors.name?.message && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.name?.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col mb-6 w-full">
+            <label
+              htmlFor="email"
+              className="text-[#5a7184] font-semibold block"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              {...register("email", {
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Enter a valid email",
+                },
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+              })}
+              placeholder="Enter email"
+              className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+                errors.email ? "border-red-500" : "border-[#c3cad9]"
+              }`}
+            />
+            {errors.email?.message && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col mb-6 w-full">
+            <label
+              htmlFor="password"
+              className="text-[#5a7184] font-semibold block"
+            >
+              New Password (optional)
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register("password")}
+              placeholder="Enter new password"
+              className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${
+                errors.password ? "border-red-500" : "border-[#c3cad9]"
+              }`}
+            />
+            {errors.password?.message && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.password?.message}
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            disabled={!isValid || profileIsLoading || updateProfileIsLoading}
+            className="bg-primary text-white font-bold text-lg py-4 px-8 w-full rounded-lg mb-6 disabled:opacity-70 disabled:cursor-not-allowed"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
